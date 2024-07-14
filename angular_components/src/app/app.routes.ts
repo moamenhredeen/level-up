779f1fe80550ec9b19c38e49c73d2ae93794447d
Moamen Hraden <moamen.hraden@primion.eu>
2024-07-14 11:56:17 +0200

    add home component

--- angular_components/src/app/app.routes.ts
+++ angular_components/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {SliderComponent} from "./slider/slider.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "slider", component: SliderComponent},
+  {path: "**", redirectTo: "home", pathMatch: "full"},
+];
